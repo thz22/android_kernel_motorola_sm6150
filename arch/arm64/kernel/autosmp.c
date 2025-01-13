bdb59294df332a765d716379591175a57bbde9ee
Felipe <164790527+felipeskyy@users.noreply.github.com>
2025-01-13 19:24:13 +0000

    arm64: kernel: autosmp: Update CPU clusters settings

--- arch/arm64/kernel/autosmp.c
+++ arch/arm64/kernel/autosmp.c
@@ -65,10 +65,10 @@ static struct asmp_param_struct {
 } asmp_param = {
     .delay = 1, // looks like .delay parameter 100 is work efficient for old device, change it to 1 ms
     .scroff_single_core = true,
-    .max_cpus_bc = 4, /* Max cpu Big cluster ! */
+    .max_cpus_bc = 3, /* Max cpu Big cluster ! */
     .max_cpus_lc = 4, /* Max cpu Little cluster ! */
-    .min_cpus_bc = 1, /* Minimum Big cluster online */
-    .min_cpus_lc = 1, /* Minimum Little cluster online */
+    .min_cpus_bc = 0, /* Minimum Big cluster online */
+    .min_cpus_lc = 2, /* Minimum Little cluster online */
     .cpufreq_up_bc = 80,
     .cpufreq_up_lc = 70,
     .cpufreq_down_bc = 25,
